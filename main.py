c60fe6673662a0c206bc81e97e164959d003b496
ash2916 <tomxaviour23@gmail.com>
2022-06-07 16:45:34 -0400

    Finished Latency Testing

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from util.model_utility import ModelUtility
 
 useGPU = False
 device = torch.device("cuda:0" if (useGPU and torch.cuda.is_available()) else "cpu")
-# print(latency.run_model(device, model_utility.contextnet(device=device)))
-latency.compare_all()
+# print(latency.get_latency(device, model_utility.contextnet(device=device)))
+latency.compare_all(use_gpu=True)
 
 # contextnet = ModelUtility(model=model_utility.contextnet(device=device).model, model_name="ContextNet",
 # dataset="LibriSpeech", num_epochs=10, device=device)
--- util/latency.py
+++ util/latency.py
@@ -5,16 +5,15 @@ from util import model_utility
 
 
 @torch.no_grad()
-def run_inference(model, *argv):
+def run_inference(device, model, *argv):
+    model.to(device)
+    model.eval()
     return model.forward(*argv)
 
 
-def run_model(device, model_dict):
-    num_repeats = 15
-    num_threads = torch.get_num_threads()
-
+def get_inference_params(device, model_dict):
     globals_dict = {}
-    stmt = "run_inference("
+    stmt = "run_inference(device, "
     for key, value in model_dict.items():
         globals_dict[key] = value
         stmt = stmt + key + ", "
@@ -22,8 +21,18 @@ def run_model(device, model_dict):
     model.to(device)
     model.eval()
     globals_dict['model'] = model
+    globals_dict['device'] = device
 
     stmt = stmt + ")"
+
+    return globals_dict, stmt
+
+
+def get_latency(device, model_dict):
+    num_repeats = 15
+    num_threads = torch.get_num_threads()
+
+    globals_dict, stmt = get_inference_params(device, model_dict)
 
     # https://pytorch.org/docs/stable/_modules/torch/utils/benchmark/utils/common.html#Measurement
     timer = benchmark.Timer(stmt=stmt,
@@ -37,29 +46,59 @@ def run_model(device, model_dict):
     return f"Latency: {profile_result.mean * 1000:.5f} ms"
 
 
-def compare_all():
+def compare_all(use_gpu=False):
     # Compare takes a list of measurements which we'll save in results.
     results = []
     devices = [torch.device("cpu"), ]
-    if torch.cuda.is_available():
+    if torch.cuda.is_available() and use_gpu:
         devices.append(torch.device("cuda:0"))
     for device in devices:
         # label and sub_label are the rows
         # description is the column
         label = 'Latency'
         sub_label = f'{device}'
-        for num_threads in [1, 4, 16]:
+        for num_threads in [1, 4, 16, 32]:
+            globals_dict, stmt = get_inference_params(device, model_utility.contextnet(device=device))
             results.append(benchmark.Timer(
-                stmt="run_model(device, model_dict)",
-                setup='from util.latency import run_model',
-                globals={"device": device, "model_dict": model_utility.contextnet(device=device)},
+                stmt=stmt,
+                setup='from util.latency import run_inference',
+                globals=globals_dict,
                 num_threads=num_threads,
                 label=label,
                 sub_label=sub_label,
                 description='ContextNet',
             ).blocked_autorange(min_run_time=1))
-            if device == "cuda:0":
-                torch.cuda.empty_cache()
+            globals_dict, stmt = get_inference_params(device, model_utility.quartznet(device=device))
+            results.append(benchmark.Timer(
+                stmt=stmt,
+                setup='from util.latency import run_inference',
+                globals=globals_dict,
+                num_threads=num_threads,
+                label=label,
+                sub_label=sub_label,
+                description='QuartzNet',
+            ).blocked_autorange(min_run_time=1))
+            globals_dict, stmt = get_inference_params(device, model_utility.conformer(device=device))
+            results.append(benchmark.Timer(
+                stmt=stmt,
+                setup='from util.latency import run_inference',
+                globals=globals_dict,
+                num_threads=num_threads,
+                label=label,
+                sub_label=sub_label,
+                description='Conformer',
+            ).blocked_autorange(min_run_time=1))
+            globals_dict, stmt = get_inference_params(device, model_utility.jasper(device=device))
+            results.append(benchmark.Timer(
+                stmt=stmt,
+                setup='from util.latency import run_inference',
+                globals=globals_dict,
+                num_threads=num_threads,
+                label=label,
+                sub_label=sub_label,
+                description='Jasper',
+            ).blocked_autorange(min_run_time=1))
+            torch.cuda.empty_cache()
 
     compare = benchmark.Compare(results)
     compare.print()
